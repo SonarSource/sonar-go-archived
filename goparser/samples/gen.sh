65491d1fd8585f0450006a4502561464c21e5d5b
Janos Gyerik <janos.gyerik@sonarsource.com>
2018-02-28 17:34:16 +0100

    DO NOT MERGE -- Add samples

---
+++ goparser/samples/gen.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if ! type goparser &>/dev/null; then
+    echo "The program 'goparser' doesn't exist or it's not on your path."
+    echo "Add ~/go/bin to your PATH, and install goparser with 'go install' inside cmd/goparser"
+    exit 1
+fi
+
+cd $(dirname "$0")
+
+for f in ../../go-samples/*.go; do
+    json=$(basename "$f").uast.json
+    if ! goparser "$f" > "$json" 2>/dev/null; then
+        echo "Error: could not parse $f. Run goparser manually on it to investigate."
+        rm -f "$json"
+    fi
+done
