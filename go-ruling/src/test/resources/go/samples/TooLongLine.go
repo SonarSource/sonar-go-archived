d822210b2d4ace9562a3ae7053a43e1f63c721e3
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2018-03-06 14:02:22 +0100

    Move samples

--- go-ruling/src/test/resources/go/samples/TooLongLine.go
+++
@@ -1,8 +0,0 @@
-// S103
-package samples
-
-func longLine() int {
-  x := 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
-  // Noncompliant@+1 {{Split this 122 characters long line (which is greater than 120 authorized).}}
-  return x + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
-}
