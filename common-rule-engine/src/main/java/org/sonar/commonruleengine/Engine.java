d061c6e8ae0d1cf8321e22933d47785f3278a30c
Alban Auzeill <alban.auzeill@sonarsource.com>
2018-03-09 18:11:24 +0100

    Add scanner scripts for demo, before being a real SonarQube plugin

--- common-rule-engine/src/main/java/org/sonar/commonruleengine/Engine.java
+++ common-rule-engine/src/main/java/org/sonar/commonruleengine/Engine.java
@@ -1,8 +1,9 @@
 package org.sonar.commonruleengine;
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,31 +14,19 @@ import org.sonar.uast.UastNode;
 
 public class Engine {
 
-  public static final List<Check> ALL_CHECKS;
-
-  static {
-    ALL_CHECKS = CheckList.getChecks().stream().map(c -> {
-      try {
-        return c.newInstance();
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
-    }).collect(Collectors.toList());
-  }
-
   private final EngineContext engineContext;
   private final MetricsVisitor metricsVisitor;
 
-  public static void main(String[] args) {
-    try {
-      BufferedReader inputReader = Files.newBufferedReader(Paths.get(args[0]));
-      UastNode uast = Uast.from(inputReader);
-      Engine engine = new Engine();
-      List<Issue> issues = engine.scan(uast).issues;
-      issues.forEach(System.out::println);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+  public static void main(String[] args) throws IOException {
+    report(System.out, new Parameters(args));
+  }
+
+  static void report(PrintStream out, Parameters params) throws IOException {
+    UastNode uast = Uast.from(Files.newBufferedReader(params.uastPath));
+    Engine engine = new Engine();
+    ScanResult scanResult = engine.scan(uast);
+    IssueReport report = new IssueReport(scanResult, params.goSourcePath, params.color, params.metrics);
+    report.writeTo(out);
   }
 
   private static List<Check> initChecks() {
@@ -88,4 +77,51 @@ public class Engine {
       this.metrics = metrics;
     }
   }
+
+  static class Parameters {
+    boolean color = false;
+    boolean metrics = false;
+    Path uastPath = null;
+    Path goSourcePath = null;
+
+    public Parameters(String[] args) {
+      for (String arg : args) {
+        switch (arg) {
+          case "--color":
+            color = true;
+            break;
+          case "--metrics":
+            metrics = true;
+            break;
+          default:
+            if (uastPath == null) {
+              uastPath = Paths.get(arg);
+            } else if (goSourcePath == null) {
+              goSourcePath = Paths.get(arg);
+            } else {
+              fatal("Too many file names provided.");
+            }
+            break;
+        }
+      }
+      if (uastPath == null) {
+        fatal("Missing parameters. Syntax: [--color] [--metrics] uast.json source.go");
+      }
+      if (goSourcePath == null) {
+        fatal("Missing go source file, you need to provide uast path and go file path.");
+      }
+      if (!Files.exists(uastPath)) {
+        fatal("File not found: " + uastPath);
+      }
+      if (!Files.exists(goSourcePath)) {
+        fatal("File not found: " + goSourcePath);
+      }
+    }
+  }
+
+  private static void fatal(String error) {
+    System.err.println("[ERROR] " + error);
+    System.exit(1);
+  }
+
 }
--- common-rule-engine/src/test/java/org/sonar/commonruleengine/EngineTest.java
+++ common-rule-engine/src/test/java/org/sonar/commonruleengine/EngineTest.java
@@ -1,6 +1,12 @@
 package org.sonar.commonruleengine;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +15,7 @@ import org.sonar.commonruleengine.checks.Check;
 import org.sonar.uast.Uast;
 import org.sonar.uast.UastNode;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -28,6 +35,21 @@ class EngineTest {
     List<Issue> issues = engine.scan(uast).issues;
     assertEquals(4, issues.size());
     assertTrue(issues.stream().map(Issue::getRule).allMatch(rule -> rule == nodeCounter));
+  }
+
+  @Test
+  void report() throws IOException {
+    Path testFolder = Paths.get("src","test","resources",
+      "org","sonar","commonruleengine","checks","go","NoSelfAssignmentCheck");
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    Engine.report(new PrintStream(out,true, UTF_8.name()), new Engine.Parameters(new String[]{
+        "--metrics",
+        testFolder.resolve("NoSelfAssignmentCheck.go.uast.json").toString(),
+        testFolder.resolve("NoSelfAssignmentCheck.go").toString()
+      }));
+    String actual = new String(out.toByteArray(), UTF_8).replace('\\', '/');
+    String expected = new String(Files.readAllBytes(testFolder.resolve("NoSelfAssignmentCheck.report.txt")), UTF_8);
+    assertEquals(expected, actual);
   }
 
   static class NodeCounter extends Check {
