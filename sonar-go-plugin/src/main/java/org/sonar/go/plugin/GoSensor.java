16b746b99676394f98fb1d285404dcc9de35519a
Alban Auzeill <alban.auzeill@sonarsource.com>
2018-03-14 14:10:29 +0100

    Send metrics to SonarQube

--- sonar-go-plugin/src/main/java/org/sonar/go/plugin/GoSensor.java
+++ sonar-go-plugin/src/main/java/org/sonar/go/plugin/GoSensor.java
@@ -1,27 +1,35 @@
 package org.sonar.go.plugin;
 
 import java.io.IOException;
+import java.io.Serializable;
 import org.sonar.api.batch.fs.FileSystem;
 import org.sonar.api.batch.fs.InputFile;
 import org.sonar.api.batch.fs.TextRange;
+import org.sonar.api.batch.measure.Metric;
 import org.sonar.api.batch.rule.CheckFactory;
 import org.sonar.api.batch.rule.Checks;
 import org.sonar.api.batch.sensor.Sensor;
 import org.sonar.api.batch.sensor.SensorContext;
 import org.sonar.api.batch.sensor.SensorDescriptor;
 import org.sonar.api.batch.sensor.issue.NewIssue;
+import org.sonar.api.measures.CoreMetrics;
+import org.sonar.api.measures.FileLinesContext;
+import org.sonar.api.measures.FileLinesContextFactory;
 import org.sonar.commonruleengine.Engine;
 import org.sonar.commonruleengine.Issue;
+import org.sonar.commonruleengine.Metrics;
 import org.sonar.commonruleengine.checks.Check;
 import org.sonar.uast.UastNode;
 
 public class GoSensor implements Sensor {
 
   private final Checks<Check> checks;
+  private final FileLinesContextFactory fileLinesContextFactory;
 
-  public GoSensor(CheckFactory checkFactory) {
+  public GoSensor(CheckFactory checkFactory, FileLinesContextFactory fileLinesContextFactory) {
     checks = checkFactory.<Check>create(GoRulesDefinition.REPOSITORY_KEY)
       .addAnnotatedChecks((Iterable) GoChecks.getChecks());
+    this.fileLinesContextFactory = fileLinesContextFactory;
   }
 
   @Override
@@ -41,6 +49,7 @@ public class GoSensor implements Sensor {
         UastNode uast = uastGenerator.createUast(inputFile);
         Engine.ScanResult scanResult = ruleEngine.scan(uast);
         scanResult.issues.forEach(issue -> reportIssue(issue, context, inputFile));
+        saveMetrics(scanResult.metrics, context, inputFile);
       }
     } catch (IOException e) {
       throw new GoPluginException("Error during analysis. Last analyzed file: \"" + lastAnalyzedFile + "\"", e);
@@ -54,6 +63,27 @@ public class GoSensor implements Sensor {
     newIssue
       .at(newIssue.newLocation().on(inputFile).at(textRange).message(issue.getMessage()))
       .forRule(checks.ruleKey(issue.getRule()))
+      .save();
+  }
+
+  private void saveMetrics(Metrics metrics, SensorContext context, InputFile inputFile) {
+    saveMetric(context, inputFile, CoreMetrics.NCLOC, metrics.linesOfCode.size());
+    saveMetric(context, inputFile, CoreMetrics.COMMENT_LINES, metrics.commentLines.size());
+    saveMetric(context, inputFile, CoreMetrics.CLASSES, metrics.numberOfClasses);
+    saveMetric(context, inputFile, CoreMetrics.FUNCTIONS, metrics.numberOfFunctions);
+    saveMetric(context, inputFile, CoreMetrics.STATEMENTS, metrics.numberOfStatements);
+
+    FileLinesContext fileLinesContext = fileLinesContextFactory.createFor(inputFile);
+    metrics.linesOfCode.forEach(line -> fileLinesContext.setIntValue(CoreMetrics.NCLOC_DATA_KEY, line, 1));
+    metrics.commentLines.forEach(line -> fileLinesContext.setIntValue(CoreMetrics.COMMENT_LINES_DATA_KEY, line, 1));
+    fileLinesContext.save();
+  }
+
+  private static <T extends Serializable> void saveMetric(SensorContext context, InputFile inputFile, Metric metric, T value) {
+    context.<T>newMeasure()
+      .on(inputFile)
+      .forMetric(metric)
+      .withValue(value)
       .save();
   }
 
