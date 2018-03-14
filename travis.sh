1cd63135853c2c71325c1f1ccdc4117a4bc64a92
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2018-03-14 15:05:36 +0100

    Analyze on next

---
+++ travis.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+function configureTravis {
+  mkdir -p ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v47 | tar zx --strip-components 1 -C ~/.local
+  source ~/.local/bin/install
+}
+configureTravis
+
+# configure environment variables for Artifactory
+export GIT_COMMIT=$TRAVIS_COMMIT
+export BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  export GIT_BRANCH=$TRAVIS_BRANCH
+  unset PULL_REQUEST_BRANCH_TARGET
+  unset PULL_REQUEST_NUMBER
+else
+  export GIT_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
+  export PULL_REQUEST_BRANCH_TARGET=$TRAVIS_BRANCH
+  export PULL_REQUEST_NUMBER=$TRAVIS_PULL_REQUEST
+fi
+
+gradle_cmd="./gradlew --no-daemon --console plain"
+sonar_analysis="-DbuildNumber=$BUILD_NUMBER \
+        -Dsonar.host.url=$SONAR_HOST_URL
+        -Dsonar.login=$SONAR_TOKEN \
+        -Dsonar.analysis.buildNumber=$BUILD_NUMBER \
+        -Dsonar.analysis.pipeline=$BUILD_NUMBER \
+        -Dsonar.analysis.sha1=$TRAVIS_PULL_REQUEST_SHA \
+        -Dsonar.analysis.repository=$TRAVIS_REPO_SLUG "
+
+
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    echo 'Build and analyze master'
+    ruling=true ${gradle_cmd} check sonarqube artifactoryPublish \
+        ${sonar_analysis} \
+        -Dsonar.projectVersion=$INITIAL_VERSION \
+
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
+    echo 'Build and analyze pull request'
+    ruling=true ${gradle_cmd} check sonarqube artifactoryPublish \
+        ${sonar_analysis} \
+        -Dsonar.analysis.prNumber=$TRAVIS_PULL_REQUEST \
+        -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST \
+        -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH \
+        -Dsonar.pullrequest.base=$TRAVIS_BRANCH \
+        -Dsonar.pullrequest.provider=github \
+        -Dsonar.pullrequest.github.repository=$TRAVIS_REPO_SLUG
+else
+    echo 'Build feature branch or external pull request'
+    ${gradle_cmd} -DbuildNumber=$BUILD_NUMBER check artifactoryPublish
+fi
+
