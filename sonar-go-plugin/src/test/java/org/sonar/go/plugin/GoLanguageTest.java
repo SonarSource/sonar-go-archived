f587a388163ec38d158c0e77f6992b7ca2353119
Alban Auzeill <alban.auzeill@sonarsource.com>
2018-04-05 11:37:45 +0200

    Support "sonar.go.file.suffixes" property. (#206)

--- sonar-go-plugin/src/test/java/org/sonar/go/plugin/GoLanguageTest.java
+++ sonar-go-plugin/src/test/java/org/sonar/go/plugin/GoLanguageTest.java
@@ -20,14 +20,24 @@
 package org.sonar.go.plugin;
 
 import org.junit.jupiter.api.Test;
+import org.sonar.api.config.internal.MapSettings;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GoLanguageTest {
 
   @Test
-  public void should_have_correct_file_extensions() throws Exception {
-    GoLanguage typeScriptLanguage = new GoLanguage();
+  void should_have_correct_file_extensions() {
+    MapSettings mapSettings = new MapSettings();
+    GoLanguage typeScriptLanguage = new GoLanguage(mapSettings.asConfig());
     assertThat(typeScriptLanguage.getFileSuffixes()).containsExactly(".go");
+  }
+
+  @Test
+  void can_override_file_extensions() {
+    MapSettings mapSettings = new MapSettings();
+    mapSettings.setProperty("sonar.go.file.suffixes", ".go1,.go2");
+    GoLanguage typeScriptLanguage = new GoLanguage(mapSettings.asConfig());
+    assertThat(typeScriptLanguage.getFileSuffixes()).containsExactly(".go1",".go2");
   }
 }
