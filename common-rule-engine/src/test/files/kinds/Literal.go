c1c5b340f701f65c921af26cebe385e297241893
Alban Auzeill <alban.auzeill@sonarsource.com>
2018-04-09 17:50:24 +0200

    Add Go parser kinds: FLOAT_LITERAL, INT_LITERAL, DECIMAL_LITERAL, HEX_LITERAL, OCTAL_LITERAL, CHAR_LITERAL

---
+++ common-rule-engine/src/test/files/kinds/Literal.go
@@ -0,0 +1,27 @@
+package main
+
+import "file.go" // Noncompliant {{LITERAL,STRING_LITERAL}}
+//     ^^^^^^^^^
+
+const N = 'a' // Noncompliant {{LITERAL,CHAR_LITERAL}}
+//        ^^^
+
+func foo() {
+  bar(42) // Noncompliant {{LITERAL,INT_LITERAL,DECIMAL_LITERAL}}
+//    ^^
+
+  a := 10 // Noncompliant {{LITERAL,INT_LITERAL,DECIMAL_LITERAL}}
+//     ^^
+  b := 010 // Noncompliant {{LITERAL,INT_LITERAL,OCTAL_LITERAL}}
+//     ^^^
+  c := 0x10 // Noncompliant {{LITERAL,INT_LITERAL,HEX_LITERAL}}
+//     ^^^^
+  d := 1.0 // Noncompliant {{LITERAL,FLOAT_LITERAL}}
+//     ^^^
+  e := "10" // Noncompliant {{LITERAL,STRING_LITERAL}}
+//     ^^^^
+  f := '1' // Noncompliant {{LITERAL,CHAR_LITERAL}}
+//     ^^^
+  g := true // Noncompliant {{LITERAL,BOOLEAN_LITERAL}}
+//     ^^^^
+}
