ac30eb29d467b9766039fa6a42da5610a9485f4a
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2018-04-26 15:34:35 +0200

    Rule S1192: String literals should not be duplicated (#278)

---
+++ common-rule-engine/src/test/files/checks/StringLiteralDuplicatedCheck/StringLiteralDuplicatedCheck.go
@@ -0,0 +1,12 @@
+package StringLiteralDuplicatedCheck
+
+func duplication(s string) {
+duplication("This literal is duplicated")  // Noncompliant {{Define a constant instead of duplicating this literal 4 times.}}
+//          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+duplication("This literal is duplicated")
+//          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ <
+duplication("This literal is duplicated")
+//          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ <
+duplication("This literal is duplicated")
+//          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ <
+}
