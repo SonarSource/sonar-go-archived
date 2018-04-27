7b3ee78d93cebf1d79daa1d232dd4d7e00eef820
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2018-04-27 14:31:15 +0200

    Rule S1763: Jump statements should not be followed by dead code (#283)

---
+++ common-rule-engine/src/main/java/org/sonar/commonruleengine/checks/UnreachableCodeCheck.java
@@ -0,0 +1,52 @@
+/*
+ * SonarQube Go Plugin
+ * Copyright (C) 2018-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.commonruleengine.checks;
+
+import java.util.List;
+import java.util.ListIterator;
+import org.sonar.check.Rule;
+import org.sonar.uast.UastNode;
+
+@Rule(key = "S1763")
+public class UnreachableCodeCheck extends Check {
+
+  public UnreachableCodeCheck() {
+    super(UastNode.Kind.BLOCK);
+  }
+
+  @Override
+  public void visitNode(UastNode node) {
+    List<UastNode> statements = node.getChildren(UastNode.Kind.STATEMENT);
+    ListIterator<UastNode> stmtIterator = statements.listIterator();
+    while (stmtIterator.hasNext()) {
+      if (stmtIterator.next().is(UastNode.Kind.UNCONDITIONAL_JUMP)) {
+        break;
+      }
+    }
+    if (stmtIterator.hasNext()) {
+      UastNode jump = stmtIterator.previous();
+      stmtIterator.next();
+      UastNode stmt = stmtIterator.next();
+      if (stmt.isNot(UastNode.Kind.LABEL)) {
+        reportIssue(jump, "Refactor this piece of code to not have any dead code after this " + jump.firstToken().value + ".");
+      }
+    }
+  }
+}
---
+++ common-rule-engine/src/test/files/checks/UnreachableCodeCheck/UnreachableCodeCheck.go
@@ -0,0 +1,38 @@
+package UnreachableCodeCheck
+
+import "fmt"
+
+func add(x, y int) int {
+	return x + y // Noncompliant {{Refactor this piece of code to not have any dead code after this return.}}
+	z := x + y
+	fmt.Print(z)
+	return 0
+}
+
+func loop() {
+	for i := 1; i < 10; i++ {
+		fmt.Print(i)
+		break // Noncompliant {{Refactor this piece of code to not have any dead code after this break.}}
+		fmt.Print(i)
+	}
+}
+
+func mandatory_return() int {
+	panic("No namespace syscall support") // Noncompliant {{Refactor this piece of code to not have any dead code after this panic.}}
+	return 0                              // this was necessary in some old go version, but it's not the case anymore
+}
+
+func ignore_labels() {
+	return
+label:
+	fmt.Print("labeled statement")
+	fmt.Print("labeled statement")
+}
+
+func switchcase(x int) {
+	switch x {
+	case 1:
+		return // Noncompliant
+		fmt.Print("Hello")
+	}
+}
