60cb20cdd51d7c2f0ecca000e42a88d1f1e36ffb
vilchik-elena <elena.vilchik@sonarsource.com>
2018-04-30 12:42:50 +0200

    Exclude 'vendor' folder with new propery sonar.go.exclusions

---
+++ sonar-go-plugin/src/test/java/org/sonar/go/plugin/GoExclusionsFileFilterTest.java
@@ -0,0 +1,91 @@
+/*
+ * SonarQube Go Plugin
+ * Copyright (C) 2018-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.go.plugin;
+
+import org.junit.jupiter.api.Test;
+import org.sonar.api.batch.fs.internal.DefaultInputFile;
+import org.sonar.api.batch.fs.internal.TestInputFileBuilder;
+import org.sonar.api.config.internal.MapSettings;
+
+import static org.junit.Assert.assertEquals;
+
+class GoExclusionsFileFilterTest {
+
+  @Test
+  public void should_exclude_vendor_dir() throws Exception {
+    MapSettings settings = new MapSettings();
+    settings.setProperty(GoPlugin.EXCLUSIONS_KEY, GoPlugin.EXCLUSIONS_DEFAULT_VALUE);
+    GoExclusionsFileFilter filter = new GoExclusionsFileFilter(settings.asConfig());
+    assertEquals(true, filter.accept(inputFile("file.go")));
+    assertEquals(false, filter.accept(inputFile("vendor/file.go")));
+    assertEquals(false, filter.accept(inputFile("vendor/someDir/file.go")));
+    assertEquals(false, filter.accept(inputFile("someDir/vendor/file.go")));
+  }
+
+  @Test
+  public void should_exclude_only_go() throws Exception {
+    MapSettings settings = new MapSettings();
+    settings.setProperty(GoPlugin.EXCLUSIONS_KEY, GoPlugin.EXCLUSIONS_DEFAULT_VALUE);
+    GoExclusionsFileFilter filter = new GoExclusionsFileFilter(settings.asConfig());
+    assertEquals(false, filter.accept(inputFile("vendor/file.go")));
+    assertEquals(true, filter.accept(inputFile("vendor/file.json")));
+  }
+
+  @Test
+  public void should_include_vendor_when_property_is_overridden() throws Exception {
+    MapSettings settings = new MapSettings();
+
+    settings.setProperty(GoPlugin.EXCLUSIONS_KEY, "");
+    GoExclusionsFileFilter filter = new GoExclusionsFileFilter(settings.asConfig());
+
+    assertEquals(true, filter.accept(inputFile("file.go")));
+    assertEquals(true, filter.accept(inputFile("vendor/file.go")));
+    assertEquals(true, filter.accept(inputFile("vendor/someDir/file.go")));
+    assertEquals(true, filter.accept(inputFile("someDir/vendor/file.go")));
+  }
+
+  @Test
+  public void should_exclude_using_custom_path_regex() throws Exception {
+    MapSettings settings = new MapSettings();
+
+    settings.setProperty(GoPlugin.EXCLUSIONS_KEY, "**/lib/**");
+    GoExclusionsFileFilter filter = new GoExclusionsFileFilter(settings.asConfig());
+
+    assertEquals(true, filter.accept(inputFile("file.go")));
+    assertEquals(true, filter.accept(inputFile("vendor/file.go")));
+    assertEquals(false, filter.accept(inputFile("lib/file.go")));
+    assertEquals(false, filter.accept(inputFile("someDir/lib/file.go")));
+  }
+
+  @Test
+  public void should_ignore_empty_path_regex() throws Exception {
+    MapSettings settings = new MapSettings();
+    settings.setProperty(GoPlugin.EXCLUSIONS_KEY, "," + GoPlugin.EXCLUSIONS_DEFAULT_VALUE + ",");
+    GoExclusionsFileFilter filter = new GoExclusionsFileFilter(settings.asConfig());
+
+    assertEquals(true, filter.accept(inputFile("file.go")));
+    assertEquals(false, filter.accept(inputFile("vendor/file.go")));
+  }
+
+  private DefaultInputFile inputFile(String file) {
+    return new TestInputFileBuilder("test","test_vendor/" + file).setLanguage(file.split("\\.")[1]).build();
+  }
+
+}
