45e5417bc5a4755ec4323539182065fed3c1dca8
David Rautureau <david.rautureau@sonarsource.com>
2018-11-02 14:13:26 +0100

    Use Travis utils v51

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 function configureTravis {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v48 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v51 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
